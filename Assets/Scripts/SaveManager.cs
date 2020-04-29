fe9651107f262ef0ac8e855d36cbd952626c1fd3
Andrea <andrea.antillon.b@gmail.com>
2020-04-29 09:03:54 -0600

    save file system implemented.

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+
+/* [System.Serializable]
+public class Stats {
+	public int level;
+
+	public Stats(int level) {
+		this.level = level;
+	}
+} */
+
+[System.Serializable]
+public class GameData {
+    public int level;
+
+    /* public GameData(int level) {
+        this.level = level;
+    } */
+    public GameData() {
+
+    }
+}
+
+public class SaveManager : MonoBehaviour
+{
+    GameData gameData;
+
+    public GameObject levelText;
+
+    Text levelTextComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameData = new GameData();
+        gameData.level = 1;
+
+        levelTextComponent = levelText.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlusOne() {
+        Debug.Log("+1");
+        gameData.level++;
+
+        levelTextComponent.text = gameData.level.ToString();
+    }
+
+    public void Save() {
+
+        string fileLocation = Application.persistentDataPath + "/savefile.json";
+        FileStream file;
+
+        if(File.Exists(fileLocation)) {
+            //File exists, open.
+            file = File.OpenWrite(fileLocation);
+        } else {
+            //File doesn't exists, create.
+            file = File.Create(fileLocation);
+        }
+
+        DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(GameData));
+        jsonSerializer.WriteObject(file, gameData);
+
+        file.Close();
+        Debug.Log("Saved to location: " + fileLocation);
+    }
+
+    public void Load() {
+        string fileLocation = Application.persistentDataPath + "/savefile.json";
+        FileStream file;
+        
+            if (File.Exists(fileLocation)) {
+                //The file already exists. Read.
+                file = File.OpenRead(fileLocation);
+            } else {
+                //The file doesn't exist. Error.
+                Debug.LogError("File not found: " + fileLocation);
+                return;
+            }
+
+            /* BinaryFormatter binaryFormatter = new BinaryFormatter();
+            stats = (Stats) binaryFormatter.Deserialize(file); */
+
+            DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(GameData));
+            gameData = (GameData) jsonSerializer.ReadObject(file);
+
+            file.Close();
+            Debug.Log("Loaded file: " + fileLocation);
+
+            levelTextComponent.text = gameData.level.ToString();
+
+    }
+}
